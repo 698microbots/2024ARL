e8757f64eb5ee1f03330449f11c724666604ea8b
mskirthik <46059837+mskirthik@users.noreply.github.com>
2024-02-06 16:25:06 -0700

    created autoPosition

--- src/main/java/frc/robot/commands/AutoPosition.java
+++ src/main/java/frc/robot/commands/AutoPosition.java
@@ -7,17 +7,17 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.LimeLightSubsystem;
 
-public class AutoCenter extends Command {
-// Instance Variables
-private double hypot = 0;
-private double turnAngle = 0;
-private double orientationAngle = 90 - turnAngle;
-private double xDisp = 0;
-private double zDisp = 0; 
-private LimeLightSubsystem limeLightSubsystem = new LimeLightSubsystem();
+public class AutoPosition extends Command {
+  // instance variables
+  private double hypot = 0;
+  private double turnAngle = 0;
+  private double orientationAngle = 90 - turnAngle;
+  private double zDisp = 0; 
+  private double xDisp = 0;
+  private LimeLightSubsystem limeLightSubsystem = new LimeLightSubsystem();
 
-  /** Creates a new AutoCenter. */
-  public AutoCenter(LimeLightSubsystem limeLightSubsystem) {
+  /** Creates a new AutoPosition. */
+  public AutoPosition(LimeLightSubsystem limeLightSubsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.limeLightSubsystem = limeLightSubsystem;
     addRequirements(limeLightSubsystem);
@@ -28,32 +28,22 @@ private LimeLightSubsystem limeLightSubsystem = new LimeLightSubsystem();
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    hypot = limeLightSubsystem.calculateZdistance();
+
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    xDisp = limeLightSubsystem.calculateXdistance();
     zDisp = limeLightSubsystem.calculateZdistance();
-    
-    // tell the motors to move & turn
-
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if(xDisp == 0 && zDisp == 0) {
-      return true;
-    } else {
-      return false;
-    }
+    return false;
   }
 }
