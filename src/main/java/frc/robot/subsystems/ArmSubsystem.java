bf8b688f9d93e479fe7204f7a13fdfb965010613
mskirthik <46059837+mskirthik@users.noreply.github.com>
2024-02-14 18:00:14 -0700

    initialized the bore encoder (Quadrature mode)

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -6,22 +6,30 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ArmSubsystem extends SubsystemBase {
+  // new instance var for distcnce travelled
+  private double distance = 0;
   /** Creates a new ArmSubsystem. */
   private final TalonFX armMotor = new TalonFX(Constants.armMotor);
+  // Initializes an encoder on DIO pins 0 and 1
+  // 2X decoding and non-inverted
+  Encoder encoder = new Encoder(0, 1, false, Encoder.EncodingType.k1X);
+
   public ArmSubsystem() {
 
   }
-  
-  public void moveArm(double speed){
+
+  public void moveArm(double speed) {
     armMotor.set(speed * .5);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    // Gets the distance traveled
+    distance = encoder.getDistance();
   }
 }
