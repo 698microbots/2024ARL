5b3f94b0d54ccd0615d1bfa943a6d0aa35e7a0fb
mskirthik <46059837+mskirthik@users.noreply.github.com>
2024-03-13 10:41:50 -0700

    made the DriveSlowMode command

--- src/main/java/frc/robot/commands/DriveSlowMode.java
+++ src/main/java/frc/robot/commands/DriveSlowMode.java
@@ -4,9 +4,21 @@
 
 package frc.robot.commands;
 
+import java.util.function.Supplier;
+
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.CommandSwerveDrivetrain;
+import frc.robot.subsystems.LimeLightSubsystem;
 
 public class DriveSlowMode extends Command {
+  private PIDController pidControllerCenter = new PIDController(.04, 0, 0.001); // kp as 0.05 works, everything else as 0
+  private CommandSwerveDrivetrain drivetrain;
+  private final SwerveRequest.FieldCentric swerveCentric = new SwerveRequest.FieldCentric(); // might change this to swerve centric
+  private Supplier<Double> xSpeed, ySpeed;
+
   /** Creates a new SlowMode. */
   public DriveSlowMode() {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -14,17 +26,21 @@ public class DriveSlowMode extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    
+    double xSpeed = this.xSpeed.get() * .5;
+    double ySpeed = this.ySpeed.get() * .5;
+    drivetrain.setControl(swerveCentric.withVelocityX(xSpeed).withVelocityY(ySpeed));
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
