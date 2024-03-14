e676a69079030a9ac4fdce4861dff2dbea1efffe
Rewand727 <atran255@gmail.com>
2024-03-14 09:53:04 -0700

    rest of manual auton

--- src/main/java/frc/robot/commands/AUTOTESTmove.java
+++ src/main/java/frc/robot/commands/AUTOTESTmove.java
@@ -55,7 +55,10 @@ public class AUTOTESTmove extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    driveTrain.setControl(swerveRequest.withVelocityX(0).withVelocityY(0).withRotationalRate(0));
+    
+  }
 
   // Returns true when the command should end.
   @Override
